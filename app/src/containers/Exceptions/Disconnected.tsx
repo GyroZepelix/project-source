18cb4051332154bbf2127a8e5f4eb7986bbeae38
GyroZepelix <gamingskullgamer@gmail.com>
2023-04-16 13:42:02 +0200

    Clear boilerplate in Disconnected.tsx

--- app/src/containers/Exceptions/Disconnected.tsx
+++ app/src/containers/Exceptions/Disconnected.tsx
@@ -2,7 +2,7 @@ import ProjectSourceLogo from '../../assets/projectsource.svg'
 
 const Disconnected = () => {
   return (
-    <div className='font-inter font-bold flex flex-row font-inter h-screen w-screen bg-primary-750' >
+    <div className='font-bold flex flex-row font-inter h-screen w-screen bg-primary-750' >
       <div className='flex flex-row justify-center items-center w-full'>
         <div className='flex flex-col justify-center items-center'>
           <img className='h-48 animate-bounce mb-4' src={ProjectSourceLogo} alt="Project Source logo" />
